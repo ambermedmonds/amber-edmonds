6679f9465478b42790796385369d7cc6c4322d97
ambermedmonds <a.m.edmonds@wustl.edu>
2026-04-02 21:35:27 -0500

    update

--- script.js
+++ script.js
@@ -33,34 +33,6 @@ if (topNav && navToggle && navMenu) {
     }
 }
 
-const filterButtons = document.querySelectorAll('.project-tags .tag')
-const cards = document.querySelectorAll('.card')
-
-if (filterButtons.length > 0) {
-    for (const button of filterButtons) {
-        button.addEventListener('click', () => {
-            const filter = button.dataset.type
-
-            // update is-active state
-            for (const b of filterButtons) {
-                b.classList.remove('is-active')
-            }
-            button.classList.add('is-active')
-
-            for (const card of cards) {
-                const types = card.dataset.type
-                const shouldShow = filter === 'all' || types.includes(filter)
-                
-                if (shouldShow) {
-                    card.style.display = ''
-                } else {
-                    card.style.display = 'none'
-                }
-            }
-        })
-    }
-}
-
 const projectVisualColumns = document.querySelectorAll('.project-case-right')
 
 if (projectVisualColumns.length > 0) {
